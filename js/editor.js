fc6da2d48e7cf2031646ba5eec48b488887bb5d7
Marijn Haverbeke <marijnh@gmail.com>
2009-10-30 10:18:21 +0100

    add contentChanged method to editor, use that for triggering line-number updates

--- js/editor.js
+++ js/editor.js
@@ -457,6 +457,7 @@ var Editor = (function(){
     importCode: function(code) {
       this.history.push(null, null, asEditorLines(code));
       this.history.reset();
+      this.contentChanged();
     },
 
     // Extract the code from the editor.
@@ -531,6 +532,7 @@ var Editor = (function(){
                         {node: line, offset: this.history.textAfter(line).length},
                         content);
       this.addDirtyNode(line);
+      this.contentChanged();
       this.scheduleHighlight();
     },
 
@@ -562,6 +564,7 @@ var Editor = (function(){
         this.container.insertBefore(makePartSpan(lines[i], doc), before);
       }
       this.addDirtyNode(line);
+      this.contentChanged();
       this.scheduleHighlight();
     },
 
@@ -631,6 +634,7 @@ var Editor = (function(){
       lines[lines.length - 1] = lastLine + this.history.textAfter(to.node).slice(to.offset);
       var end = this.history.nodeAfter(to.node);
       this.history.push(from.node, end, lines);
+      this.contentChanged();
       return {node: this.history.nodeBefore(end),
               offset: lastLine.length};
     },
@@ -1023,9 +1027,6 @@ var Editor = (function(){
         this.container.createTextRange().execCommand("unlink");
         this.selectionSnapshot = select.getBookmark(this.container);
       }
-      if (window.frameElement && window.frameElement.CodeMirror.updateNumbers) {
-        window.frameElement.CodeMirror.updateNumbers();
-      }
 
       var activity = this.options.cursorActivity;
       if (!safe || activity) {
@@ -1034,9 +1035,16 @@ var Editor = (function(){
         cursor = cursor || this.container.firstChild;
         if (activity) activity(cursor);
         if (!safe) {
+          this.contentChanged();
           this.scheduleHighlight();
           this.addDirtyNode(cursor);
         }
+      }
+    },
+
+    contentChanged: function() {
+      if (window.frameElement && window.frameElement.CodeMirror.updateNumbers) {
+        window.frameElement.CodeMirror.updateNumbers();
       }
     },
 
