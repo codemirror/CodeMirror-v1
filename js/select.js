codemirror/CodeMirror-v1

commit f870e9e8dd1deee06eafbe7c1ab361cb5be9d4a6

solve problem with backwards-selections. said problem was in turn caused by an exception added to a hack to solve this problem in the first place. said exception is there to work around a webkit bug, and as such this latest issue only manifested itself on webkit. etc.

js/select.js
@@ -413,7 +413,16 @@ var select = {};
 
     select.selectMarked = function () {
       var cs = currentSelection;
-      if (!(cs && (cs.changed || (webkit && cs.start.node == cs.end.node)))) return;
+      // on webkit-based browsers, it is apparently possible that the
+      // selection gets reset even when a node that is not one of the
+      // endpoints get messed with. the most common situation where
+      // this occurs is when a selection is deleted or overwitten. we
+      // check for that here.
+      function focusIssue() {
+        return cs.start.node == cs.end.node && cs.start.offset == 0 &&
+          cs.end.offset == (cs.end.node ? cs.end.node.nodeValue.length : 0);
+      }
+      if (!cs || !(cs.changed || (webkit && focusIssue()))) return;
       var win = cs.window, range = win.document.createRange();
 
       function setPoint(point, which) {

commit 08d2d67c66a3250353c79974399b24458499a425

fix bug introduced in commit f870e9e8dd1 that broke pasting (and probably some other things) on empty lines in Chrome

js/select.js
@@ -419,8 +419,7 @@ var select = {};
       // this occurs is when a selection is deleted or overwitten. we
       // check for that here.
       function focusIssue() {
-        return cs.start.node == cs.end.node && cs.start.offset == 0 &&
-          cs.end.offset == (cs.end.node ? cs.end.node.nodeValue.length : 0);
+        return cs.start.node == cs.end.node && cs.start.offset == 0 && cs.end.offset == 0;
       }
       if (!cs || !(cs.changed || (webkit && focusIssue()))) return;
       var win = cs.window, range = win.document.createRange();