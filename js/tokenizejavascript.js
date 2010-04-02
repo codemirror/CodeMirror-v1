131f7cec1b771a10c3c6a27c262c6fce1389cc08
jasongrout <jason-github@creativetrax.com>
2010-04-01 21:16:27 -0700

    Fixed typo in a comment

--- js/tokenizejavascript.js
+++ js/tokenizejavascript.js
@@ -65,7 +65,7 @@ var tokenizeJavaScript = (function() {
     };
   }
 
-  // The token reader, inteded to be used by the tokenizer from
+  // The token reader, intended to be used by the tokenizer from
   // tokenize.js (through jsTokenState). Advances the source stream
   // over a token, and returns an object containing the type and style
   // of that token.
