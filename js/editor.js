cbe01e3760e3856950264eaeda66911d383036e6
Marijn Haverbeke <marijnh@gmail.com>
2010-09-04 12:52:07 +0200

    add check against window.parent to timeout-backout code, because recent versions of Opera raise errors when a CodeMirror instance is removed from the document with timeouts pending

--- js/editor.js
+++ js/editor.js
@@ -1075,7 +1075,7 @@ var Editor = (function(){
         unhighlight(self.highlighted[1]);
       }
 
-      if (!window.select) return;
+      if (!window.parent || !window.select) return;
       // Clear the event property.
       if (this.parenEvent) this.parent.clearTimeout(this.parenEvent);
       this.parenEvent = null;
@@ -1271,7 +1271,7 @@ var Editor = (function(){
     highlightDirty: function(force) {
       // Prevent FF from raising an error when it is firing timeouts
       // on a page that's no longer loaded.
-      if (!window.select) return false;
+      if (!window.parent || !window.select) return false;
 
       if (!this.options.readOnly) select.markSelection(this.win);
       var start, endTime = force ? null : time() + this.options.passTime;
@@ -1291,7 +1291,7 @@ var Editor = (function(){
       var self = this, pos = null;
       return function() {
         // FF timeout weirdness workaround.
-        if (!window.select) return;
+        if (!window.parent || !window.select) return;
         // If the current node is no longer in the document... oh
         // well, we start over.
         if (pos && pos.parentNode != self.container)
