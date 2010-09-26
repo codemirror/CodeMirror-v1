31cdeb72802e675e62a81bf9d2243a7fd6752a3a
Mike Brevoort <mike@brevoort.com>
2010-09-26 15:34:08 -0600

    applied license headers, cleanup

--- contrib/xquery/js/tokenizexquery.js
+++ contrib/xquery/js/tokenizexquery.js
@@ -1,3 +1,26 @@
+/*
+Copyright 2010 Mike Brevoort http://mike.brevoort.com (twitter:@mbrevoort)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This is an indirect collective derivative of the other parses in this package
+
+*/
+
+// A tokenizer for xQuery, basically trying to look at the source stream, 
+// chunk into proper tokens and apply the correct metadata to be able to
+// apply the proper CSS class
+
 var tokenizeXquery = (function() {
     // Advance the stream until the given character (not preceded by a
     // backslash) is encountered, or the end of the line is reached.
