e1c496411ae6e44e619baa804b1cccfdb2e86c9c
Marijn Haverbeke <marijnh@gmail.com>
2010-10-11 12:11:48 +0200

    load extra font lazily, to make pages more responsive and keep offline version functional

---
+++ css/font.js
@@ -0,0 +1,14 @@
+function waitForStyles() {
+  for (var i = 0; i < document.styleSheets.length; i++)
+    if (/googleapis/.test(document.styleSheets[i].href))
+      return document.body.className += " droid";
+  setTimeout(waitForStyles, 100);
+}
+setTimeout(function() {
+  var link = document.createElement("LINK");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = "http://fonts.googleapis.com/css?family=Droid+Sans|Droid+Sans:bold";
+  document.documentElement.getElementsByTagName("HEAD")[0].appendChild(link);
+  waitForStyles();
+}, 20);
