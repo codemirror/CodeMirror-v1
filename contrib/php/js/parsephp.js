82184a21fc341d9ca9d0de4e6949562e284d8d78
Marijn Haverbeke <marijnh@gmail.com>
2011-01-10 12:38:07 +0100

    support 'global , , ;' in PHP parser

--- contrib/php/js/parsephp.js
+++ contrib/php/js/parsephp.js
@@ -253,9 +253,8 @@ var PHPParser = Editor.Parser = (function() {
         else
           ok = wanted.indexOf(type);
         if (ok >= 0) {
-          if (execute && typeof(execute[ok]) == "function")
-            execute[ok](token);
-            cont();  // just consume the token
+          if (execute && typeof(execute[ok]) == "function") pass(execute[ok]);
+          else cont();
         }
         else {
           if (!marked) mark(token.style);
@@ -281,7 +280,8 @@ var PHPParser = Editor.Parser = (function() {
       else if (type == "foreach") cont(pushlex("form"), require("("), pushlex(")"), expression, require("as"), require("variable"), /* => $value */ expect(")"), altsyntax, poplex, statement, poplex);
       else if (type == "for") cont(pushlex("form"), require("("), pushlex(")"), expression, require(";"), expression, require(";"), expression, require(")"), altsyntax, poplex, statement, poplex);
       // public final function foo(), protected static $bar;
-      else if (type == "modifier") cont(require(["modifier", "variable", "function", "abstract"], [null, null, funcdef, absfun]));
+      else if (type == "modifier") cont(require(["modifier", "variable", "function", "abstract"],
+                                                [null, commasep(require("variable")), funcdef, absfun]));
       else if (type == "abstract") abs();
       else if (type == "switch") cont(pushlex("form"), require("("), expression, require(")"), pushlex("}", "switch"), require([":", "{"]), block, poplex, poplex);
       else if (type == "case") cont(expression, require(":"));
